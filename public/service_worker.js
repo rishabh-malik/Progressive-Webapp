495cd22e86c952daebce1a2955de4483488e239a
rishabh-malik <rishabhmalik249@gmail.com>
2017-10-16 16:58:26 +0530

    caching and fetching offline

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,7 +1,40 @@
-self.addEventListener('install',function(){
+self.addEventListener('install',function(event){
     console.log('service worker installed');
+    //waits until cache are saved
+    event.waitUntil(
+         //caching things
+         //static cache
+        caches.open('static')
+            .then(function(cache){
+            // cache.add('/');
+            // cache.add('/index.html');
+            // cache.add('/src/js/app.js');
+            //OR
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/css/app.css',
+                '/src/images/pwa.jpg',
+                'https://fonts.googleapis.com/css?family=Raleway:400,700'
+        ]);
+    }));
+    
 });
 
 self.addEventListener('activate',function(){
     console.log('service worker activated');
+});
+
+self.addEventListener('fetch',function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(res){
+                if(res){
+                    return res;
+                } else{
+                    return fetch(event.request);
+                }
+        })
+    );
 });
